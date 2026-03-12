2e9f9c6af223f9ae4c5110e13c43116065a53221
tuorn <nicolas.tuor@unifr.ch>
2026-03-12 14:27:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Remplace "ia-news-ethique-ecole" par le VRAI nom de ton dépôt GitHub s'il est différent !
+  base: '/ia-news-ethique-ecole/' 
 })
